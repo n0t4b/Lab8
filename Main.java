10127aa0fed51f263e4d74c2ebd1f243eee9d789
Nikita Vashchenko~ <strangerstr71@stud.op.edu.ua>
2024-11-14 11:56:12 +0100

    Add method to calculate sum of two numbers

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ package vashchenko.ad231;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Sum: " + add(5, 10));
+    }
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
+
